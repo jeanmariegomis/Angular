e80a8de699e4a36ae3d2c0fa86c2773bfd894c7b
jeanmariegomis <gomisj170@gmail.com>
2019-08-07 08:35:48 +0000

    source

---
+++ Bureau/fatigue/src/Controller/EntrepriseController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Depot;
+use App\Entity\Compte;
+use App\Form\DepotType;
+use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\DatetimeInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/** 
+ * @Route("/api")
+ */
+class EntrepriseController extends AbstractController
+{
+    /** 
+     * @Route("/entreprise", name="entreprise", methods={"POST"})
+     */
+    public function enregistrer(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder,ValidatorInterface $validator,SerializerInterface $serializer): Response
+    {
+        
+        $values = json_decode($request->getContent());
+        $random=random_int(100000,999999);
+        $user= new User();
+        $user->setUsername($values->username);
+        $user->setPassword($passwordEncoder->encodePassword($user, $values->password));
+        $user->setNom($values->nom);
+        $user->setPrenom($values->prenom);
+        $user->setEmail($values->email);
+        $user->setTelephone($values->telephone);
+        $user->setNci($values->nci);
+        $user->setStatus($values->status);
+        $user->setRoles(array('ROLE_ADMIN'));
+      
+
+        $entreprise= new Entreprise();
+        $entreprise->setRaisonsociale($values->raisonsociale);
+        $entreprise->setNinea($values->ninea);
+        $entreprise->setAdresse($values->adresse);
+        $entreprise->setStatus($values->status);
+        $user->setEntreprise($entreprise);
+
+
+        $compte= new Compte();
+        $compte->setNumcompte($random);
+        $compte->setSolde($values->solde);
+        $compte->setEntreprise($entreprise);
+        $entityManager = $this->getDoctrine()->getManager();
+        $errors = $validator->validate($depot);
+            if (count($errors)) {
+                $errors = $serializer->serialize($errors, 'json');
+                return new Response($errors, 500, ['Content-Type' => 'Application/json']);
+            }
+        $entityManager->persist($user);
+        $entityManager->persist($entreprise);
+        $entityManager->persist($compte);
+        $entityManager->flush();
+
+        
+          
+        return new Response('L\'entreprise a été ajouté',Response::HTTP_CREATED); 
+    }
+
+
+     /** 
+     * @Route("/depot", name="depot", methods={"POST"})
+     */
+
+
+    public function depot (Request $request, UserInterface $Userconnecte, ValidatorInterface $validator, SerializerInterface $serializer) // UserInterface permette de recuperer l'utilisateur actuellement connecté
+    {
+        $depot = new Depot();//creation d'un objet de type depot
+       
+            $values = json_decode($request->getContent());
+            $repo = $this->getDoctrine()->getRepository(Compte::class);// recupere le repository compte
+            $compte = $repo->findOneBy(['numcompte' => $values->numcompte]);//findOneBy(['numcompte' => $values->numcompte]recherche 
+            
+           $depot->setDate(new \DateTime());// on remplit la date du depot a l'instant t
+           $depot->setUser($Userconnecte);// liaison du caissier avec depot
+           $depot->setMontant($values->montant);
+           $depot->setSoldeactuele($values->soldeactuele);
+           $depot->setCompte($compte);
+           $compte->setSolde($compte->getSolde()+$depot->getMontant());// on rempli le nouveau solde du depot
+           $manager=$this->getDoctrine()->getManager();// recuperation de l'objet manager
+           $errors = $validator->validate($depot);
+            if (count($errors)) {
+                $errors = $serializer->serialize($errors, 'json');
+                return new Response($errors, 500, ['Content-Type' => 'Application/json']);
+            }
+           $manager->persist($compte);// nous permet d'ecrire dans la table entreprise
+           $manager->persist($depot);//permet d'ecrire dans la table depot
+           $manager->flush();
+           $data = [
+               'status' => 201,
+               'message' => 'Le depot a bien été effectué '
+           ];
+           return new JsonResponse($data, 201);// on retourne l'objet JSON
+
+    
+        
+    }
+
+}
---
+++ Bureau/fatigue/src/Entity/Compte.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\CompteRepository")
+ */
+class Compte
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numcompte;
+
+    /**
+     * @ORM\Column(type="bigint")
+     *  @Assert\Range(
+     * min = 75000,
+     * minMessage = "Le depot doit etre minimum 75000"
+     * )
+     */
+    private $solde;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Entreprise", inversedBy="comptes")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $entreprise;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Depot", mappedBy="compte")
+     */
+    private $depots;
+
+    public function __construct()
+    {
+        $this->depots = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumcompte(): ?int
+    {
+        return $this->numcompte;
+    }
+
+    public function setNumcompte(int $numcompte): self
+    {
+        $this->numcompte = $numcompte;
+
+        return $this;
+    }
+
+    public function getSolde(): ?int
+    {
+        return $this->solde;
+    }
+
+    public function setSolde(int $solde): self
+    {
+        $this->solde = $solde;
+
+        return $this;
+    }
+
+    public function getEntreprise(): ?Entreprise
+    {
+        return $this->entreprise;
+    }
+
+    public function setEntreprise(?Entreprise $entreprise): self
+    {
+        $this->entreprise = $entreprise;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Depot[]
+     */
+    public function getDepots(): Collection
+    {
+        return $this->depots;
+    }
+
+    public function addDepot(Depot $depot): self
+    {
+        if (!$this->depots->contains($depot)) {
+            $this->depots[] = $depot;
+            $depot->setCompte($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDepot(Depot $depot): self
+    {
+        if ($this->depots->contains($depot)) {
+            $this->depots->removeElement($depot);
+            // set the owning side to null (unless already changed)
+            if ($depot->getCompte() === $this) {
+                $depot->setCompte(null);
+            }
+        }
+
+        return $this;
+    }
+}
