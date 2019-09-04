24c79c42462d78c1194382663c3cc7c218c68ddf
jeanmariegomis <gomisj170@gmail.com>
2019-09-04 13:05:25 +0000

    ajout

---
+++ Sommeil/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EntrepriseComponent } from './entreprise/entreprise.component';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
+
+const routes: Routes = [
+{path: '',redirectTo: '/login',pathMatch: 'full'},
+{path: 'login',component: LoginComponent},
+{path: 'entreprise',component: EntrepriseComponent},
+{path: 'utilisateur',component: UtilisateurComponent},
+{path: 'menu',component: MenuComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
